5095b8b06a0e203c34710894966fa8d36309461e
iPappyTech <pappythorium@gmail.com>
2022-09-15 15:29:37 +0100

    made chnages to index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ rightBackid.innerHTML = `
     <h3> ${rightBack.position}</h3>
     <h1> ${rightBack.jerseyNumber}</h1>
     <h2> ${rightBack.playerName}</h2>
-
 </div>
 `
 var centerBack2 = new Club("Varane", "6", "CB")
@@ -31,7 +30,6 @@ centerBack2Id.innerHTML = `
         <h3> ${centerBack2.position}</h3>
         <h1> ${centerBack2.jerseyNumber}</h1>
         <h2> ${centerBack2.playerName}</h2>
-
     </div>
 `
 var centerBack1 = new Club("Matinez", "5", "CB")
@@ -40,7 +38,6 @@ centerBack1Id.innerHTML = `
         <h3> ${centerBack1.position}</h3>
         <h1> ${centerBack1.jerseyNumber}</h1>
         <h2> ${centerBack1.playerName}</h2>
-
     </div>
 `
 var leftBack = new Club("Malacia", "3", "LB")
@@ -49,6 +46,13 @@ leftBackid.innerHTML = `
         <h3> ${leftBack.position}</h3>
         <h1> ${leftBack.jerseyNumber}</h1>
         <h2> ${leftBack.playerName}</h2>
-
     </div>
 `
+var defensiveMid = new Club("Fred", "4", "DM")
+var attackingMid1 = new Club("Eriksen", "10", "AM")
+var attackingMid2 = new Club("Bruno", "8", "AM")
+var leftWinger = new Club("Anthony", "11", "LW")
+var rightWinger = new Club("Sancho", "7", "RW")
+
+
+
