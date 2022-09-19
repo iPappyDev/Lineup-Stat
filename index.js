94072efefd26ba2185586fa58a7e528d30e32066
iPappyTech <pappythorium@gmail.com>
2022-09-19 13:07:19 +0100

    added new files

--- index.js
+++ index.js
@@ -48,11 +48,53 @@ leftBackid.innerHTML = `
         <h2> ${leftBack.playerName}</h2>
     </div>
 `
-var defensiveMid = new Club("Fred", "4", "DM")
+var defensiveMid = new Club("Fred", "4", "DM-")
+defensiveMidf.innerHTML = `
+<div id="keeps">
+    <h1> ${defensiveMid.jerseyNumber}</h1>
+   <div id="dispf">
+        <h3> ${defensiveMid.position}</h3>
+        <h3> ${defensiveMid.playerName}</h3>
+   </div>
+</div>
+`
 var attackingMid1 = new Club("Eriksen", "10", "AM")
+attackingMidf1.innerHTML = `
+    <div id="Back1">
+        <h3> ${attackingMid1.position}</h3>
+        <h1> ${attackingMid1.jerseyNumber}</h1>
+        <h2> ${attackingMid1.playerName}</h2>
+    </div>
+`
 var attackingMid2 = new Club("Bruno", "8", "AM")
+attackingMidf2.innerHTML = `
+    <div id="Back1">
+        <h3> ${attackingMid2.position}</h3>
+        <h1> ${attackingMid2.jerseyNumber}</h1>
+        <h2> ${attackingMid2.playerName}</h2>
+    </div>
+`
 var leftWinger = new Club("Anthony", "11", "LW")
+leftWing.innerHTML = `
+    <div id="Back1">
+        <h3> ${leftWinger.position}</h3>
+        <h1> ${leftWinger.jerseyNumber}</h1>
+        <h2> ${leftWinger.playerName}</h2>
+    </div>
+`
 var rightWinger = new Club("Sancho", "7", "RW")
-
-
-
+rightWing.innerHTML = `
+    <div id="Back1">
+        <h3> ${rightWinger.position}</h3>
+        <h1> ${rightWinger.jerseyNumber}</h1>
+        <h2> ${rightWinger.playerName}</h2>
+    </div>
+`
+var forward = new Club("Rashford", "10", "ST")
+striker.innerHTML = `
+<div id="Back1">
+    <h3> ${forward.position}</h3>
+    <h1> ${forward.jerseyNumber}</h1>
+    <h2> ${forward.playerName}</h2>
+</div>
+`
